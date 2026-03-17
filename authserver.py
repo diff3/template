5a3b947f396ccb10fb258374ab3e663ff77d3857
Magnus Pettersson <magnus@7300-RX7YWD9J2C.local>
2026-03-17 22:08:42 +0100

    updated loader to auth- and worldserver

--- authserver.py
+++ authserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-from server.auth import run_auth
+from server.authserver import run_auth
 
 if __name__ == "__main__":
-    run_auth(config_path=os.environ["AUTH_CONFIG"])
+    run_auth()
--- worldserver.py
+++ worldserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-from server.world import run_world
+from server.worldserver import run_world
 
 if __name__ == "__main__":
-    run_world(config_path=os.environ["SERVER_CONFIG"])
+    run_world()
